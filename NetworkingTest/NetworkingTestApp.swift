86054c58e50b5471593311128a840a37f56d266f
Uriel Ortega <urielortega2509@gmail.com>
2023-08-27 13:57:20 -0600

    Initial Commit

---
+++ NetworkingTest/NetworkingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingTestApp.swift
+//  NetworkingTest
+//
+//  Created by Uriel Ortega on 27/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
